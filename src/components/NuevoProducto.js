ca766340d3c390f00897de487f37c083c0a3ac12
Brian Lopez <brianlopezmedina12@gmail.com>
2021-06-27 13:10:56 -0500

    Update newproduct component

--- src/components/NuevoProducto.js
+++ src/components/NuevoProducto.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React from "react";
 
 const NuevoProducto = () => {
-    return (
-        <div>
-            <h1>Desde nuevoproducto</h1>
+  return (
+    <div className="row justify-content-center">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-body">
+            <h2 className="text-center mb-4 font-weight-bold">
+              Agregar Nuevo Producto
+            </h2>
+
+            <form>
+              <div className="form-group">
+                <label>Nombre Producto</label>
+                <input
+                  tyoe="text"
+                  className="form-control"
+                  placeholder="Nombre Producto"
+                  name="nombre"
+                />
+              </div>
+
+              <div className="form-group">
+                <label>Precio Producto</label>
+                <input
+                  tyoe="number"
+                  className="form-control"
+                  placeholder="Precio Producto"
+                  name="precio"
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+              >
+                Agregar
+              </button>
+            </form>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default NuevoProducto;
