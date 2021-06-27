996acf7e935b415a87fc83f35fcafa63eed541a7
Brian Lopez <brianlopezmedina12@gmail.com>
2021-06-27 13:05:11 -0500

    update new components added & routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary
-        justify-content-between">
-            <div className="container">
-                <h1>CRUD - React, Redux, REST API & Axios</h1>
-            </div>
+  return (
+    <nav
+      className="navbar navbar-expand-lg navbar-dark bg-primary
+        justify-content-between"
+    >
+      <div className="container">
+        <h1>
+          <Link to={"/"} className="text-light">
+            CRUD - React, Redux, REST API & Axios
+          </Link>
+        </h1>
+      </div>
 
-            <a href="/productos/nuevo"
-                className="btn btn-danger nuevo-post
+      <Link
+        to={"/productos/nuevo"}
+        className="btn btn-danger nuevo-post
                 d-block d-md-inline-block"
-            >Agregar Producto &#43;</a>
-        </nav>
-    );
+      >
+        Agregar Producto &#43;
+      </Link>
+    </nav>
+  );
 };
 
 export default Header;
---
+++ src/components/NuevoProducto.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NuevoProducto = () => {
+    return (
+        <div>
+            <h1>Desde nuevoproducto</h1>
+        </div>
+    );
+};
+
+export default NuevoProducto;
